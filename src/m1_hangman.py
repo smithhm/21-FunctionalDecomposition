9e065e4b963241d3637d0e1098d5dafa1f58628f
smithhm <smithhm@rose-hulman.edu>
2019-01-24 11:36:26 -0500

    M1 DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,57 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Haiden Smith and Alex Tabuyo.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
 
-####### Do NOT attempt this assignment before class! #######
+# Do NOT attempt this assignment before class! #######
+import random
 
+
+def main():
+    a = random_word()
+    c = list(len(a) * '-')
+    n = 5
+    while True:
+        n = word_guess(a, c, n)
+        if list(a) == c:
+            print("You won!")
+            break
+        if n == 0:
+            print('You lose! The word was', '"', a, '"')
+            break
+
+
+def random_word():
+    with open('words.txt') as f:
+        f.readline()
+        string = f.read()
+        words = string.split()
+    r = random.randrange(0, len(words) - 1)
+    word = words[r]
+    return word
+
+
+def guess():
+    return str(input('What letter do you want to try?'))
+
+
+def word_guess(a, c, n):
+    b = guess()
+    if b in a:
+        for k in range(len(a)):
+            if b == a[k]:
+                c[k] = b
+    else:
+        n = n - 1
+        print('You have guessed incorrectly!', n, 'remaining guesses.')
+
+    for k in range(len(c)):
+        print(c[k], end='')
+    print()
+    return n
+
+
+main()
